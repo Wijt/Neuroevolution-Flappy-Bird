d6276f6611d1bf3e0685a7484a00ae0692b4e8a2
Furkan Kaya <8fkaya@gmail.com>
2020-06-25 20:19:34 +0300

    Constants edited.

--- data/constants.js
+++ data/constants.js
@@ -3,22 +3,28 @@ BIRD_R = 25;
 BIRD_JUMP_POWER = 6;
 //#endregion
 
+//#region PIPE
 PIPE_GAP_H = 500;
 PIPE_WIDTH = 50;
 PIPE_SCROOL = 2;
 PIPE_BETWEEN = 180;
 PIPE_NO_GAP_ZONE = 150;
+//#endregion
 
+//#region GROUND
 GROUND_HEIGHT = 25;
+//#endregion
 
-
+//#region GAME SETTINGS
 DEBUG_MODE = false;
+//#endregion
 
+//#region COLOR THEME
 BIRD_COLOR = "#e43f5a";
 PIPE_COLOR = "#1f4068";
 GROUND_COLOR = "#162447"
 BG_COLOR = "#1b1b2f";
-
+//#endregion
 
 //#region PERCEPTRON
 PERCEPTRON_LEARING_RATE = 0.1;
